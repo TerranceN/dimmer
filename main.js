24dcaa4b84a1c65defec726186fe5b08a5b138ad
Terrance Niechciol <TNiechciol@gmail.com>
2016-11-05 18:11:24 -0400

    added super basic dimmer overlay

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 $(function() {
+
+  var loadDimmerOverlay = function() {
+    $('body').append($('<div id="dimmer_overlay"></div>'));
+  }
+
+  var dim = function(t) {
+    if ($('#dimmer_overlay').length == 0) {
+      loadDimmerOverlay();
+    }
+    $('#dimmer_overlay').css('opacity', t);
+  }
+
   console.log("Dimmer: Extension loaded!");
+  dim(0.5);
 });
